b40c4fa3a2bfa386fa8ada0ffb03b68fc52859e3
Tiago Freire Barbosa <tfreirebar@gmail.com>
2017-04-04 18:01:19 -0300

    Adicionando ReclyclerView e Picasso

--- app/src/main/java/tech/infofun/popularmovies/MainActivity.java
+++ app/src/main/java/tech/infofun/popularmovies/MainActivity.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private MoviesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +26,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        mAdapter = new MoviesAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        List<Movie> movies = new ArrayList<>();
+
+        for(int i = 0; i < 25; i++ ){
+            movies.add(new Movie());
+        }
+
+        mAdapter.setmMovieList(movies);
+
     }
 
     @Override
@@ -48,5 +60,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder{
+        public ImageView imageView;
+        public MovieViewHolder(View itemView){
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
     }
 }
---
+++ app/src/main/java/tech/infofun/popularmovies/MoviesAdapter.java
@@ -0,0 +1,55 @@
+package tech.infofun.popularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tfbarbosa on 04/04/2017.
+ */
+public class MoviesAdapter extends RecyclerView.Adapter<MainActivity.MovieViewHolder>{
+        private List<Movie> mMovieList;
+        private LayoutInflater mInflater;
+        private Context mContext;
+
+        public MoviesAdapter(Context context){
+            this.mContext = context;
+            this.mInflater = LayoutInflater.from(context);
+            this.mMovieList = new ArrayList<>();
+        }
+
+        @Override
+        public MainActivity.MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            View view = mInflater.inflate(R.layout.row_movie,parent,false);
+            MainActivity.MovieViewHolder viewHolder = new MainActivity.MovieViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(MainActivity.MovieViewHolder holder,int position){
+            Movie movie = mMovieList.get(position);
+
+            Picasso.with(mContext)
+                    .load(movie.getPoster())
+                    .placeholder(R.color.colorAccent)
+                    .into(holder.imageView);
+        }
+
+        @Override
+        public int getItemCount(){
+            return (mMovieList == null) ? 0 : mMovieList.size();
+        }
+
+        public void setmMovieList(List<Movie> movieList){
+            this.mMovieList.clear();
+            this.mMovieList.addAll(movieList);
+            notifyDataSetChanged();
+        }
+}
