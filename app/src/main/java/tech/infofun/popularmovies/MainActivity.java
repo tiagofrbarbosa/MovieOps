0dddc3261510e8b8e9352d887a2b2ca20e023a59
Tiago Barbosa <tfreirebar@gmail.com>
2017-04-09 00:34:17 -0300

    API KEY

--- app/src/main/java/tech/infofun/popularmovies/MainActivity.java
+++ app/src/main/java/tech/infofun/popularmovies/MainActivity.java
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
                 .setRequestInterceptor(new RequestInterceptor() {
                     @Override
                     public void intercept(RequestFacade request) {
-                        request.addEncodedQueryParam("api_key","4ef00b7823d6ac0b6eb76eba2d0727fa");
+                        request.addEncodedQueryParam("api_key","API_KEY");
                         request.addEncodedQueryParam("page",String.valueOf(nPages));
                     }
                 })
